2d9285a3661e083feb74edeeb2b22ea6a16cce36
Lonny Gomes <code@lonnygomes.com>
2020-01-20 23:39:59 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+import './main.css';
+
+console.log('hello world');
+
+const vid = document.querySelector('.video');
+const btn = document.getElementById('toggle');
+btn.addEventListener('click', () => {
+    vid.play();
+})
